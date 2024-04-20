1aee8476d06ade26f5e3d5904782156bc5b87f35
pacific006 <146131662+pacific006@users.noreply.github.com>
2024-04-20 11:41:48 +0000

    object

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -16,12 +16,41 @@ const regularUser={
     }
 }
 
-console.log(regularUser.fullname.usefullname.firstname);
+// console.log(regularUser.fullname.usefullname.firstname);
 
 const obj1={1:"a",2:"b"}
 
 const obj2={3:"c",4:"d"}
 //const obj3={obj1,obj2}//object inside obj
 //const obj3=Object.assign({},obj1,obj2)//{}=>destination
-const obj3={...obj1,...obj3}
-console.log(obj3);
+const obj3={...obj1, ...obj2}
+// console.log(obj3);
+
+///destructuring of objects
+
+const course={
+    coursename:"js",
+    price:"999",
+    courseInstructtor:"hariram"
+}
+
+//course.courseInstructtor
+
+/*to make code more cleaner*/
+
+
+const {courseInstructtor:insructor} =course
+
+//console.log(courseInstructtor);
+// console.log(insructor);
+
+
+
+
+
+
+
+
+
+
+
