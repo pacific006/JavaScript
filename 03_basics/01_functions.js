2e7063d0fbf5a3fb4c8b9130cfb4a82dcf9c0850
pacific006 <146131662+pacific006@users.noreply.github.com>
2024-04-20 12:16:59 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -39,4 +39,34 @@ function loginUserMessage(username)
     return `${username} welcome`
 }
 
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+/*when we dont know the number of argument user is going to send*/
+function calculateCartPrice(...num1)//rest operator
+{
+    return num1;
+
+}
+
+// console.log(calculateCartPrice(200,400,500,600,7000,8000))
+
+const user={
+    username:"hitesh",
+    price:199
+}
+
+function handleObject(anyobject)
+{
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+
+}
+
+handleObject(user)
+
+const arr=[200,300,400,500]
+
+function returnSecondValue(getValue)
+{
+    return getValue[1]
+}
+
+console.log(returnSecondValue(arr));
